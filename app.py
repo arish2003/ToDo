cac648f2530837dd83d94976e32ea9539fca4f53
Arish Manasia <arish.manasia2003@gmail.com>
2024-08-27 13:32:41 +0530

    First Commit

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ToDo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key_here'  # Required for flash messages
 db = SQLAlchemy(app)
-app.app_context().push()
 
 class ToDo(db.Model):
     srno = db.Column(db.Integer, primary_key=True)
@@ -30,6 +30,7 @@ def index():
             )
             db.session.add(new_todo)
             db.session.commit()
+            flash('ToDo added successfully!', 'success')
         return redirect(url_for('index'))
     
     search_query = request.args.get('search', '')
@@ -51,11 +52,27 @@ def about():
 def contact():
     return render_template('contact.html')
 
+# New route to handle contact form submissions
+@app.route('/send_email', methods=['POST'])
+def send_email():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    message = request.form.get('message')
+
+    if name and email and message:
+        # You can process the form data here (e.g., send an email or log it)
+        flash('Your message has been sent successfully!', 'success')
+    else:
+        flash('All fields are required.', 'danger')
+
+    return redirect(url_for('contact'))
+
 @app.route('/complete/<int:srno>', methods=['POST'])
 def complete(srno):
     todo = ToDo.query.get_or_404(srno)
     todo.completed = True
     db.session.commit()
+    flash('ToDo marked as completed!', 'success')
     return redirect(url_for('index'))
 
 @app.route('/undo/<int:srno>', methods=['POST'])
@@ -63,6 +80,7 @@ def undo(srno):
     todo = ToDo.query.get_or_404(srno)
     todo.completed = False
     db.session.commit()
+    flash('ToDo marked as incomplete!', 'success')
     return redirect(url_for('index'))
 
 @app.route('/delete/<int:srno>', methods=['POST'])
@@ -70,7 +88,19 @@ def delete(srno):
     todo = ToDo.query.get_or_404(srno)
     db.session.delete(todo)
     db.session.commit()
+    flash('ToDo deleted successfully!', 'success')
     return redirect(url_for('index'))
+
+@app.route('/edit/<int:srno>', methods=['GET', 'POST'])
+def edit(srno):
+    todo = ToDo.query.get_or_404(srno)
+    if request.method == 'POST':
+        todo.title = request.form.get('title')
+        todo.desc = request.form.get('desc')
+        db.session.commit()
+        flash('ToDo updated successfully!', 'success')
+        return redirect(url_for('index'))
+    return render_template('edit.html', todo=todo)
 
 if __name__ == '__main__':
     app.run(debug=True)
